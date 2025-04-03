a9d9b5cad41d28d7ddd80b6cf55a20d32589e294
Bryan Luu <loc.bryan.luu@gmail.com>
2025-04-02 17:14:33 -0700

    :recycle: refactor maze builders into Graph
    Let's keep the maze related stuff in the Graph class. I should probably use better names

--- src/maze.ts
+++ src/maze.ts
@@ -217,6 +217,124 @@ class Graph<VertexType> implements Maze<VertexType> {
     // if no edges match, return false
     return false;
   }
+
+  private static buildAdjacencyGraph(mazeWidth: number, mazeHeight: number) {
+    for (let i = 0; i < (mazeWidth * mazeHeight); i++)
+      new MazeVertex();
+    let nodes = MazeVertex.nodes;
+
+    Graph.adjacencyGraph = new Graph(nodes, mazeWidth, mazeHeight);
+    let adj = Graph.adjacencyGraph;
+    let i = 0;
+    for (let r = 0; r < mazeHeight; r++) {
+      for (let c = 0; c < mazeWidth; c++) {
+        let src = MazeVertex.getNode(i);
+        let srcIndex = src.data.index;
+        let weight = Math.floor(nodes.length * Math.random());
+        if ((c % mazeWidth) > 0)
+        {
+          let left = MazeVertex.getNode(MazeVertex.getNeighborIndex(srcIndex, "left"));
+          adj.insertEdge(src, left, weight);
+        }
+        if (r > 0) {
+          let above = MazeVertex.getNode(MazeVertex.getNeighborIndex(srcIndex, "up"));
+          adj.insertEdge(src, above, weight);
+        }
+        i++;
+      }
+    }
+  }
+
+  /**
+   * Creates the Maze graph with the given dimensions
+   *
+   * @param {*} mazeWidth
+   * @param {*} mazeHeight
+   */
+  static buildMaze(mazeWidth, mazeHeight) {
+    let nodes = MazeVertex.nodes;
+    Graph.buildAdjacencyGraph(mazeWidth, mazeHeight);
+    let adj = Graph.adjacencyGraph;
+    let hasCheaperEdge = (u, v) => {
+      return v.data.cost - u.data.cost;
+    }
+    let priorityQueue = new Heap(hasCheaperEdge);
+
+    let start = MazeVertex.getNode(0); // TODO adjust for dynamic start
+    priorityQueue.insert(start);
+    Graph.mazeGraph = new Graph(nodes, mazeWidth, mazeHeight);
+    let maze = Graph.mazeGraph;
+    while (!priorityQueue.isEmpty()) {
+      let v = priorityQueue.extract(); // return border node with cheapest edge
+      v.data.used = true;
+      let cheapestCost = v.data.cost;
+      let cheapestNeighbor = MazeVertex.getNode(v.data.route);
+
+      // if v touches the maze, add cheapest neighboring edge to maze
+      if (cheapestNeighbor)
+        maze.insertEdge(v, cheapestNeighbor, cheapestCost);
+
+      // loop through outgoing edges of v
+      let neighbors = adj.weights.get(v);
+      neighbors.forEach((weight, w, _) => {
+        if ((!w.data.used) && weight < w.data.cost) {
+          w.data.cost = weight;
+          w.data.route = v.data.index;
+          priorityQueue.insert(w);
+        }
+      })
+    }
+
+    // maze.insertEdge(nodes[0], nodes[1], 1);
+    // maze.insertEdge(nodes[1], nodes[2], 1);
+    // maze.insertEdge(nodes[2], nodes[5], 1);
+    // maze.insertEdge(nodes[3], nodes[6], 1);
+    // maze.insertEdge(nodes[4], nodes[7], 1);
+    // maze.insertEdge(nodes[5], nodes[4], 1);
+    // maze.insertEdge(nodes[6], nodes[7], 1);
+    // maze.insertEdge(nodes[7], nodes[8], 1);
+  }
+
+  /**
+   * Creates the HTML for the Maze and inserts it into the document
+   */
+  static displayMaze() {
+    let g = Graph.mazeGraph;
+    let mazeWidth = g.dimensions.width;
+    let mazeHeight = g.dimensions.height;
+    let mazeHTML = "";
+    let gapHTML = '<div class="maze-tile maze-gap"></div>';
+    let makeEdgeHTML = (srcIndex, tgtIndex) => {
+      let src = MazeVertex.getNode(srcIndex);
+      let tgt = MazeVertex.getNode(tgtIndex);
+      let visible = g.isNeighbor(src, tgt);
+      let classes = (visible ? "maze-tile maze-edge maze-path" : "maze-tile maze-edge maze-gap");
+      return `<div id="e${srcIndex}-${tgtIndex}" class="${classes}"></div>`;
+    }
+    let index = 0;
+    for (let i = 0; i < 2*mazeHeight-1; i++) {
+      for (let j = 0; j < 2*mazeWidth-1; j++) {
+        if ((i % 2 == 0) && (j % 2 == 0)) { // vertex tile
+          let vertexHTML = `<div id="v${index}" class="maze-tile maze-vertex maze-path"></div>`;
+          mazeHTML += vertexHTML;
+          index++
+        } else {
+          if (i % 2 == 0) { // horizontal edge tile
+            let src = index - 1;
+            let tgt = index;
+            mazeHTML += makeEdgeHTML(src, tgt);
+          } else if (j % 2 == 0) { // vertical edge tile
+            let tgt = index + (j / 2);
+            let src = tgt - mazeWidth;
+            mazeHTML += makeEdgeHTML(src, tgt);
+          } else // gap
+            mazeHTML += gapHTML;
+        }
+      }
+    }
+    // fill the maze with tiles
+    $("#maze-grid").html(mazeHTML);
+  }
 }
 
 class Player extends VertexTile {
